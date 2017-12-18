1c6a350be564a2fafa9e984ac425de305162aa76
ZsibokMark <zsibokmark@gmail.com>
2017-12-18 20:15:09 +0100

    Két szám összeadása
    Az x és y változót összeadjuk!

---
+++ pro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 1;
+            int eredmeny = x + y;
+
+            Console.WriteLine(eredmeny);
+        }
+    }
+}
